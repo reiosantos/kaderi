2e6e94b06947bf6981711636116da0f89cbe34be
reiosantos <ronireiosantos@gmail.com>
2018-02-15 09:53:15 +0300

    all modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
 import { Component } from '@angular/core';
+import { Location } from '@angular/common';
+import {Router} from '@angular/router';
+import {AuthenticationService} from './_services/authentication.service';
+import {User} from './_models/user';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+
+	title = 'Kaderi Sales System';
+	isLoggedin: boolean;
+	user: User = null;
+
+	constructor(
+		public router: Router,
+		private authService: AuthenticationService,
+		private location: Location
+	) {
+		if (localStorage.getItem('currentUser') == null){
+			this.router.navigate(['/login']);
+		}
+		this.user = JSON.parse(localStorage.getItem('currentUser'));
+		this.isLoggedin = !!(this.user && this.user.token);
+	}
+
+	loginEmit(status) {
+		this.isLoggedin = status;
+		this.user = JSON.parse(localStorage.getItem('currentUser'));
+		if (this.isLoggedin) {
+			location.reload();
+		}
+	}
+
+	logout() {
+		this.authService.logout();
+		this.isLoggedin = false;
+		location.reload();
+	}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,64 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { AdminComponent } from './admin/admin.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {routing} from './app.routing';
+import {AlertComponent} from './_directives/alert.component';
+import {JwtInterceptor} from './_helpers/jwt.interceptor';
+import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
+import {AuthGuard} from './_guards/auth.guard';
+import {AuthenticationService} from './_services/authentication.service';
+import {AlertService} from './_services/alert.service';
+import {UserService} from './_services/user.service';
+import {ProductService} from './_services/product.service';
+import {SaleService} from './_services/sale.service';
+import {CommentService} from './_services/comment.service';
+import { AdminProductsComponent } from './admin-products/admin-products.component';
+import { AdminUsersComponent } from './admin-users/admin-users.component';
+import { AdminSalesComponent } from './admin-sales/admin-sales.component';
+import { AdminCommentsComponent } from './admin-comments/admin-comments.component';
+import { AdminStoresComponent } from './admin-stores/admin-stores.component';
+import {StoreService} from './_services/store.service';
 
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		AdminComponent,
+		HomeComponent,
+		LoginComponent,
+		AlertComponent,
+		AdminProductsComponent,
+		AdminUsersComponent,
+		AdminSalesComponent,
+		AdminCommentsComponent,
+		AdminStoresComponent
+	],
+	imports: [
+		BrowserModule,
+		FormsModule,
+		ReactiveFormsModule,
+		routing,
+		HttpClientModule,
+	],
+	providers: [
+		AuthGuard,
+		AlertService,
+		AuthenticationService,
+		UserService,
+		ProductService,
+		SaleService,
+		CommentService,
+		StoreService,
+		{
+			provide: HTTP_INTERCEPTORS,
+			useClass: JwtInterceptor,
+			multi: true
+		},
+	],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
