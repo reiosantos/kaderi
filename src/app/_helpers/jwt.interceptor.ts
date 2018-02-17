7cadfe75cf9c2b2deb9e049841c7f2350419d008
reiosantos <ronireiosantos@gmail.com>
2018-02-17 21:18:35 +0300

    commented out the added header to solve cross origin header error

--- src/app/_helpers/jwt.interceptor.ts
+++ src/app/_helpers/jwt.interceptor.ts
@@ -12,7 +12,7 @@ export class JwtInterceptor implements HttpInterceptor {
 			request = request.clone({
 				setHeaders: {
 					// Authorization: `Bearer ${currentUser.token}`
-					Authorization: `Bearer ${currentUser.token}`
+					// Authorization: `Bearer ${currentUser.token}`
 				}
 			});
 		}
